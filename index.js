90e97b2b629a4b29dc9597aa99a4e187fa0f9179
liuchao <liuchao126mail@126.com>
2020-11-15 16:22:49 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const lockBindings = require('bindings')('lockfile_bindings')
 const path = require('path')
 
 exports.lockFile = (filePath, fileContent) => {
-  let ret = false
+  let ret = -1
   if (filePath) {
     filePath = path.normalize(filePath)
     ret = lockBindings.LockFile(filePath, fileContent)
